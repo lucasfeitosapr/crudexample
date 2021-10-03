c709dd6c8f44bf507e56539726a1fe2a61eaa7c3
Lucas Feitosa <lucasfeitosapr@gmail.com>
2021-10-03 13:10:48 -0300

    Modifying controller to use exception handler

--- src/main/java/com/gebotech/crudexample/controller/TutorialController.java
+++ src/main/java/com/gebotech/crudexample/controller/TutorialController.java
@@ -1,6 +1,7 @@
 package com.gebotech.crudexample.controller;
 
 
+import com.gebotech.crudexample.exception.ResourceNotFoundException;
 import com.gebotech.crudexample.model.Tutorial;
 import com.gebotech.crudexample.repository.TutorialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,85 +23,63 @@ public class TutorialController {
 
     @GetMapping("/tutorials")
     public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
-        try {
-            List<Tutorial> tutorials = new ArrayList<Tutorial>();
 
-            if(title == null){
-                tutorialRepository.findAll().forEach(tutorials::add);
-            } else {
-                tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
-            }
+        List<Tutorial> tutorials = new ArrayList<Tutorial>();
 
-            if(tutorials.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            return new ResponseEntity<>(tutorials, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        if(title == null){
+            tutorialRepository.findAll().forEach(tutorials::add);
+        } else {
+            tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
         }
+
+        if(tutorials.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(tutorials, HttpStatus.OK);
     }
 
     @GetMapping("/tutorials/{id}")
     public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id) {
-        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+        Tutorial tutorial = tutorialRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Tutorial with id = " + id));
 
-        if (tutorialData.isPresent()) {
-            return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return new ResponseEntity<>(tutorial, HttpStatus.OK);
     }
 
     @PostMapping("/tutorials")
     public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
-        try {
-            Tutorial _tutorial = tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
-            return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        Tutorial _tutorial = tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
+        return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
     }
 
     @PutMapping("/tutorials/{id}")
     public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
-        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+        Tutorial _tutorial = tutorialRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Not found Tutorial with id = " + id));
 
-        if(tutorialData.isPresent()) {
-            Tutorial _tutorial = tutorialData.get();
-            _tutorial.setTitle(tutorial.getTitle());
-            _tutorial.setDescription(tutorial.getDescription());
-            _tutorial.setPublished(tutorial.isPublished());
+        _tutorial.setTitle(tutorial.getTitle());
+        _tutorial.setDescription(tutorial.getDescription());
+        _tutorial.setPublished(tutorial.isPublished());
 
-            return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.CREATED);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/tutorials/{id}")
     public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") long id) {
-        try {
-            tutorialRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        tutorialRepository.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/tutorials/published")
     public ResponseEntity<List<Tutorial>> findByPublished() {
-        try{
-            List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
+        List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
 
-            if(tutorials.isEmpty()){
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            return new ResponseEntity<>(tutorials, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        if(tutorials.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
+
+        return new ResponseEntity<>(tutorials, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/gebotech/crudexample/exception/ResourceNotFoundException.java
+++ src/main/java/com/gebotech/crudexample/exception/ResourceNotFoundException.java
@@ -1,2 +1,11 @@
-package com.gebotech.crudexample.exception;public class ResourceNotFoundException {
+package com.gebotech.crudexample.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+
 }
